93534ae051dc44816200c8ccf7f256c406cb395a
Alison Simião <alison.s@multiplier.com.br>
2023-08-19 00:25:20 -0300

    fix(clock): tipos

--- src/components/clock/analogic/index.tsx
+++ src/components/clock/analogic/index.tsx
@@ -11,16 +11,17 @@ const clock: React.FC<ClockProps> = ({ time: {min, hour, sec} }: ClockProps) => 
     const angleHour = (30 * hour) + (0.5 * min )
     const angleSec = (sec )
     const angleMin = (min)
+    
     return (
         <div className='container-analogic'>
             
-            <div style={{'--i': angleSec}} className='pointer sec' />
-            <div style={{'--i': angleMin}} className='pointer min' />
-            <div style={{'--i': angleHour}} className='pointer hour' />
+            <div style={{'--i': angleSec} as Record<string, number>} className='pointer sec' />
+            <div style={{'--i': angleMin} as Record<string, number>} className='pointer min' />
+            <div style={{'--i': angleHour} as Record<string, number>} className='pointer hour' />
             
             {
                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((value, index) => {
-                    const style = { '--i': index+1 };
+                    const style: Record<string, number> = { '--i': index+1 };
                     return (
                         <div style={style} className="clock-number" key={value}>
                             {value}
